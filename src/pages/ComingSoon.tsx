4701f6b9f8549f5fcdf647eae6610dd3050c1a8b
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2025-07-18 14:39:18 +0300

    update some changes

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -123,6 +123,14 @@ const ComingSoon = () => {
                     <path d="M22.5 2h-3.4L12 9.2 4.9 2H1l8.4 8.4L1 22h3.4l7.1-7.5L19 22h3.4l-8.5-9L22.5 2z"/>
                 </svg>
                 </a>
+
+                {/* Instagram */}
+                <a href="https://instagram.com/yourprofile" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-pink-600 dark:hover:text-white">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm4.25 3.25a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7zm5.25-.25a.75.75 0 1 1 0 1.5.75.75 0 0 1 0-1.5z"/>
+                </svg>
+                </a>
+
             </div>
         </div>
 
